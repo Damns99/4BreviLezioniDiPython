0624586c6f2e928fe6b4327f93099d9c67975fbb
giulio99 <56739190+giulio99@users.noreply.github.com>
2019-11-05 16:08:37 +0100

    Add files via upload

--- Lezione2/log generico.py
+++ Lezione2/log generico.py
@@ -6,9 +6,13 @@ from scipy.optimize import curve_fit
 ## Dati dal file.
 x, dx, y, dy = pylab.loadtxt('dati log.txt', unpack = True)
 
-##Modello
+##Modello lineare
 def model(x, m,q):
     return m*x+q
+
+##Modello quadratico
+def model1(x, m,q, e):
+    return m*x**e+q
     
 ##Funzione Propagazione Errori
 def errlog(x,dx):
@@ -25,32 +29,38 @@ pars, covm = curve_fit(model, xx, yy, sigma=dyy, absolute_sigma=False)
 print('m  = %.5f +- %.5f ' % (pars[0], numpy.sqrt(covm.diagonal()[0])))
 print('q  = %.5f +- %.5f ' % (pars[1], numpy.sqrt(covm.diagonal()[1])))
 
+##Determino i migliori valori di m, q, e
+pars1, covm1 = curve_fit(model1, x, y, sigma=dy, absolute_sigma=False)
+print('m1 = %.5f +- %.5f ' % (pars1[0], numpy.sqrt(covm1.diagonal()[0])))
+print('q1  = %.5f +- %.5f ' % (pars1[1], numpy.sqrt(covm1.diagonal()[1])))
+print('e  = %.5f +- %.5f ' % (pars1[2], numpy.sqrt(covm1.diagonal()[2])))
+
 
 ##Grafico principale
 
 plt.figure(1)
+plt.subplot(212)
 plt.plot(xx,yy,'.',markersize=1,color='black')
 plt.errorbar(xx, yy, dyy, dxx, fmt='none', ecolor='black')
 X=numpy.linspace(0,numpy.max(xx), 10)
 plt.plot(X,model(X, *pars), color='blue', alpha=0.5)
 plt.grid()
 
-##Modello
-def model1(x, m,q, e):
-    return m*x**e+q
+plt.subplot(211)
+plt.plot(x,y,'.',markersize=1,color='black')
+plt.errorbar(x, y, dy, dx, fmt='none', ecolor='black')
+X=numpy.linspace(0,numpy.max(x), 1000)
+plt.plot(X,model1(X, *pars1), color='blue', alpha=0.5)
+plt.grid()
 
+plt.show()
 
-##Determino i migliori valori di M e q 
-pars, covm = curve_fit(model1, x, y, sigma=dy, absolute_sigma=False)
-print('m  = %.5f +- %.5f ' % (pars[0], numpy.sqrt(covm.diagonal()[0])))
-print('q  = %.5f +- %.5f ' % (pars[1], numpy.sqrt(covm.diagonal()[1])))
-print('e  = %.5f +- %.5f ' % (pars[2], numpy.sqrt(covm.diagonal()[2])))
 
 ##Grafico in carta bilogaritmica
 plt.figure(2)
 plt.errorbar(x, y, dy, dx,linestyle='', ecolor='black')
 X=numpy.logspace(0.2,2, 1000)
-plt.loglog(X,model1(X, *pars), color='blue')
+plt.loglog(X,model1(X, *pars1), color='blue')
 plt.grid()
 
 plt.show()
